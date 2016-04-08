9e081ee170082a08abc525135c238db5b1fb166f
Ken Belva <owasp@silverbackventuresllc.com>
2016-04-08 11:42:30 -0400

    Handling of large files (>10MB) and very minor speed improvements

--- src/dataImport/belvaDataImport.py
+++ src/dataImport/belvaDataImport.py
@@ -49,40 +49,37 @@ from src.parsers.belvaParseXML import parseHTMLallText as parseAllHTMLtext
 
 
 #--------------------------------------------------------------------------
-def belvaDataImport(global_gui_status_msgs, global_gui_status_msgs_brief, global_gui_progressBar, directoryPath, MD5_string):
+def belvaDataImport(global_gui_status_msgs, global_gui_status_msgs_brief, global_gui_progressBar, small_filename_dict, MD5_string, remove_common_words):
 #--------------------------------------------------------------------------
     
-    filename_dict = {}
-    for root, directories, filenames in os.walk(directoryPath):
-        for filename in filenames:
-            full_path_w_file = os.path.join(root,filename)
-            filename, file_extension = os.path.splitext(full_path_w_file)
-            filename_dict[full_path_w_file] = file_extension
-            filename = os.path.basename(full_path_w_file)
-
 
     #-------------------
-    remove_common_words = []
-    common_words_file = os.getcwd() + "/filterDictionaries/commonwords.txt"
+#    remove_common_words = []
+#    common_words_dir = os.getcwd() + "/filterDictionaries/"
 
-    f = open(common_words_file,'r')
     
-    for line in f:
-        if str(line).strip():
-            remove_common_words.append(str(line).strip().lower())
+#    for root, directories, filenames in os.walk(common_words_dir):
+#        for filename in filenames:
+#            full_path_w_file = os.path.join(root,filename)
 
-    f.close()
-    f = None
+#            f = open(full_path_w_file,'r')
+    
+#            for line in f:
+#                if str(line).strip():
+#                    remove_common_words.append(str(line).strip().lower())
+
+#            f.close()
+#            f = None
     #-------------------
 
 
 
 
-    number_xml = sum( ".xml" in x for x in filename_dict.values())
-    number_txt = sum( ".txt" in x for x in filename_dict.values())
-    number_raw = sum( ".raw" in x for x in filename_dict.values())
+    number_xml = sum( ".xml" in x for x in small_filename_dict.values())
+    number_txt = sum( ".txt" in x for x in small_filename_dict.values())
+    number_raw = sum( ".raw" in x for x in small_filename_dict.values())
 
-    total_files = len(filename_dict)
+    total_files = len(small_filename_dict)
 
     global_gui_status_msgs.append("Total number of burp files: " + (str(number_xml)))
     global_gui_status_msgs.append("Total number of text files: " + (str(number_txt)))
@@ -104,8 +101,8 @@ def belvaDataImport(global_gui_status_msgs, global_gui_status_msgs_brief, global
     # burp
     i = 0
     count = 0
-    for full_path in filename_dict:
-        if filename_dict[full_path] == ".xml":
+    for full_path in small_filename_dict:
+        if small_filename_dict[full_path] == ".xml":
             i += 1
 
             count = global_gui_progressBar.value() + 1
@@ -117,8 +114,8 @@ def belvaDataImport(global_gui_status_msgs, global_gui_status_msgs_brief, global
 
     # text dictionaries
     i = 0
-    for full_path in filename_dict:
-        if filename_dict[full_path] == ".txt":
+    for full_path in small_filename_dict:
+        if small_filename_dict[full_path] == ".txt":
             i += 1
 
             count = global_gui_progressBar.value() + 1
@@ -130,8 +127,8 @@ def belvaDataImport(global_gui_status_msgs, global_gui_status_msgs_brief, global
 
     # ZAP
     i = 0
-    for full_path in filename_dict:
-        if filename_dict[full_path] == ".raw":
+    for full_path in small_filename_dict:
+        if small_filename_dict[full_path] == ".raw":
             i += 1
 
             count = global_gui_progressBar.value() + 1
@@ -153,17 +150,12 @@ def txtDataImport(global_gui_status_msgs, global_gui_status_msgs_brief, full_pat
     for line in f:
         if str(line).strip():
             i += 1
-            
-            is_common_word = False
-            for common_word in remove_common_words:
-                if str(line).strip() == common_word:
-                    is_common_word = True
 
-            if not is_common_word: 
+            if not(str(line).strip() in remove_common_words):
                 write_consolidated_list(str(line).strip(), MD5_string)
                 global_gui_status_msgs_brief.setText("Text File word count: " + str(i))
-
-
+            
+            
     f.close()
     f = None
 
@@ -197,12 +189,7 @@ def zapDataImport(global_gui_status_msgs, global_gui_status_msgs_brief, full_pat
 #        print(word)
         i += 1
 
-        is_common_word = False
-        for common_word in remove_common_words:
-            if str(word).strip() == common_word:
-                is_common_word = True
-
-        if not is_common_word: 
+        if not(str(word).strip() in remove_common_words):
             write_consolidated_list(word, MD5_string)
             global_gui_status_msgs_brief.setText("Zap File word count: " + str(i))
 
@@ -242,14 +229,10 @@ def belvaImportBurpXML(global_gui_status_msgs, global_gui_status_msgs_brief, dat
                 for word in all_text:
                     i += 1
 
-                    is_common_word = False
-                    for common_word in remove_common_words:
-                        if str(word).strip() == common_word:
-                            is_common_word = True
-
-                    if not is_common_word:
+                    if not(str(word).strip() in remove_common_words):
                         write_consolidated_list(word, MD5_string)
                         global_gui_status_msgs_brief.setText("burp File word count: " + str(i))
+
 
 
     global_gui_status_msgs.append("Total number of burp file words for batch: " + str(i))
